9d6fb8801ef10315b92b6637f0598e6344da150d
John Weslley <weslley.john@academico.ifpb.edu.br>
2024-11-06 21:34:56 -0300

    Create Videogame_atv.py

---
+++ Videogame_atv.py
@@ -0,0 +1,26 @@
+from datetime import date
+
+class Videogame:
+    num_serie = 1
+    def __init__(self, marca = 'playstation', modelo = 'play 5', data_fab = date.today()):
+        self.marca = marca
+        self.modelo = modelo
+        self.data_fab = data_fab
+        self.num_serie = Videogame.num_serie
+        self.jogos = ['need for stive', 'praça of world']
+        self.garantia = 5
+        Videogame.num_serie += 1
+        
+v1 = Videogame()
+v2 = Videogame(data_fab = 2022)
+v3 = Videogame(data_fab = 2022, marca = 'Nintendo', modelo = '64')
+
+games = [v1, v2, v3]
+
+for game in games:
+    print(f'Marca: {game.marca}; \nModelo: {game.modelo}; \nData de Fabricação: {game.data_fab}; \nNumero de série: {game.num_serie}; \nJogos: {game.jogos[0]}, {game.jogos[1]}; \n----------------\n\n')
+
+for game in games:
+    for atributo, valor in game.__dict__.items():
+        print(f'{atributo}: {valor}')
+    print()
